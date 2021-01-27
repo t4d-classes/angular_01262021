cd9016c2d149121a4778152e9c8a7e7007733a86
Eric Greene <eric@t4d.io>
2021-01-27 14:58:06 -0500

    add code demos

--- demo-app/src/app/app-routing.module.ts
+++ demo-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CouleurHomeComponent } from './couleur-tool/components/couleur-home/couleur-home.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: CouleurHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
